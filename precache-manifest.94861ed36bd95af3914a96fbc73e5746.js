f5d3aec3945e597a1f0471365b9c1b7237973f98
zubairshakoorarbisoft <zubair.shakoor@arbisoft.com>
2020-01-02 20:06:41 +0500

    Updates

---
+++ precache-manifest.94861ed36bd95af3914a96fbc73e5746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e9c6843354118b156484d1e0f99e089",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "40aae63367897d3164d7",
+    "url": "/pics/static/css/main.ce0f250d.chunk.css"
+  },
+  {
+    "revision": "4f0eff5d11556fe6478f",
+    "url": "/pics/static/js/2.28613c32.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/pics/static/js/2.28613c32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40aae63367897d3164d7",
+    "url": "/pics/static/js/main.3be35691.chunk.js"
+  },
+  {
+    "revision": "a486d975594a83fabef1",
+    "url": "/pics/static/js/runtime-main.5707d73b.js"
+  }
+]);
